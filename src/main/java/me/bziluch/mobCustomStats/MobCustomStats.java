3571d4e0e5a1f4e1d13c275da687191e4295ad3d
Bziluch <bziluch@gmail.com>
2024-08-29 21:54:56 +0200

    initial commit

---
+++ src/main/java/me/bziluch/mobCustomStats/MobCustomStats.java
@@ -0,0 +1,17 @@
+package me.bziluch.mobCustomStats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobCustomStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
